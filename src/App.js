a1e9b62f78c6d98945e2de72c7bf3c063fad9a96
Liam0312 <0312liam@gmail.com>
2023-03-09 19:09:54 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import About from "./components/About";
+import Reservations from "./components/Reservations";
+import OrderOnline from "./components/OrderOnline";
+import Login from "./components/Login";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Header />}/>
+          <Route path="/about" element={<About />} />
+          <Route path="/reservations" element={<Reservations />} />
+          <Route path="orderOnline" element={<OrderOnline />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
